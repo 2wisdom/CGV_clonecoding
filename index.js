0093381b62727fd732372a8df7b8d069a4011f96
이지혜 <2_2wisdom@ijihyeui-MacBookPro.local>
2022-07-28 00:57:59 +0900

    hover_fail

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function closeBanner() {
     document.querySelector('.banner').style.display = 'none'
     document.querySelector('.btn_ad_close').style.display = 'none'
 }
+
+// special
+function special_hover() {
+    document
+        .querySelectorAll('.cine_livingroom')
+        .addEventListener('mouseover', (e) => {
+            document.querySelector('.special_img').src =
+                'https://img.cgv.co.kr//Front/Main/2022/0616/16553622935690.png'
+        })
+}
