10eb241ea46a752ff64d747a1a60d62a20fd7b25
2wisdom <leejihye7117@gmail.com>
2022-08-18 00:02:20 +0900

    banner

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // banner close
 function closeBanner() {
-  document.querySelector(".banner").style.display = "none";
+  document.querySelector(".banner_close").style.display = "none";
   document.querySelector(".btn_ad_close").style.display = "none";
 }
 
